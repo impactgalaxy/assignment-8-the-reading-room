996af2c23f36a9aee6c2cfb63006998cb9caaf3b
impactgalaxy <traderglow@gmail.com>
2024-03-26 00:32:44 +0600

    initial setup complete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-8xl playfair'>Vite + React</h1>
+      <button className='btn work-sans'>Add</button>
+
+    </>
+  )
+}
+
+
+
+
+export default App
